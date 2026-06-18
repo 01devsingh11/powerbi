37a60eb8545aa3b0428c3b68926f95181327245c
Amit Singh <kumaramit.cpr121@gmail.com>
2026-06-18 22:28:42 +0530

    added basic deatils

---
+++ main.py
@@ -0,0 +1,33 @@
+# Print a message
+print("Hello, World!")
+
+# Variables
+name = "Amit"
+age = 24
+
+# Print variables
+print("Name:", name)
+print("Age:", age)
+
+# Take user input
+user_name = input("Enter your name: ")
+
+# Display input
+print("Welcome,", user_name)
+
+# Simple function
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print("Sum:", result)
+
+# Loop
+for i in range(1, 6):
+    print("Number:", i)
+
+# Conditional
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
